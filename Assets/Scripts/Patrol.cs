29153574904d119451bcaa54ecdc55d4275eb2ba
Kelvin Ngo <kelvinngosos@gmail.com>
2021-01-09 15:45:09 -0800

    Patrol Script

--- Assets/Scripts/Patrol.cs
+++ Assets/Scripts/Patrol.cs
@@ -15,7 +15,7 @@ public class Patrol : MonoBehaviour
 
         transform.Translate(Vector2.right * speed * Time.deltaTime);
 
-        RaycastHit2d groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, 2f);
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, distance);
         if(groundInfo.collider == false){
             if(movingRight == true){
                 transform.eulerAngles = new Vector3(0, -180, 0);
