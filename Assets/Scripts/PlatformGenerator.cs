76b09c1f83ab5a98e9ead5a9348f314cdeafd399
Tim-02 <59761189+Tim-02@users.noreply.github.com>
2021-01-09 17:52:46 -0800

    Random x platforms

--- Assets/Scripts/PlatformGenerator.cs
+++ Assets/Scripts/PlatformGenerator.cs
@@ -9,6 +9,7 @@ public class PlatformGenerator : MonoBehaviour
     public float distanceBetween;
 
     private float platformHeight = 1;
+    private float screenWidth = 9;
 
     // Start is called before the first frame update
     void Start()
@@ -21,7 +22,7 @@ public class PlatformGenerator : MonoBehaviour
     {
         if(transform.position.y < genPoint.position.y)
         {
-            transform.position = new Vector3(0, transform.position.y + distanceBetween + platformHeight, 0);
+            transform.position = new Vector3(Random.Range(-screenWidth/2, screenWidth/2), transform.position.y + distanceBetween + platformHeight, 0);
 
             Instantiate(platform, transform.position, transform.rotation);
         }
