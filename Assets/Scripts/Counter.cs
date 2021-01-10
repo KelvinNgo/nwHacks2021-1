15d1860508e872560f571d13b73c3da8cdbce07b
Nicholas Johnston <nicholas.johnston94@gmail.com>
2021-01-10 09:59:34 -0800

    added counter text to main gameplay

---
+++ Assets/Scripts/Counter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Counter : MonoBehaviour
+{
+    Counter text;
+    // InitialX integer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // InitialX = 0;
+        text = GetComponent<Counter>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
