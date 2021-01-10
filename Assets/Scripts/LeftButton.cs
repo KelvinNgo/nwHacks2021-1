4c81fe11a9d3d1a97b3ae55650ee1b21ae381d46
Tristan Martinuson <tmartinuson@gmail.com>
2021-01-10 13:13:14 -0600

    Working Jump button

---
+++ Assets/Scripts/LeftButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LeftButton : MonoBehaviour
+{
+    public Button leftButton;
+    public Player player;
+
+    void Start()
+    {
+        Button btn = leftButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        float move = -6;
+        player.Move(move);
+    }
+}
