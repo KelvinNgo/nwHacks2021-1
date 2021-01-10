f7e8beb9e3a93fb41057af3082e94a1e2640d46d
Kelvin Ngo <kelvinngosos@gmail.com>
2021-01-09 19:27:13 -0800

    Falling Hazards Scripts

---
+++ Assets/Scripts/FallingHazard.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingHazard : MonoBehaviour
+{
+    public GameObject HazardBlock;
+    private float SpawnHazardTimer = 2f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        InstantiateHazard();
+    }
+
+    void InstantiateHazard()
+    {
+        SpawnHazardTimer -= Time.deltaTime;
+        if(SpawnHazardTimer <= 0) {
+            Instantiate (HazardBlock, new Vector2(-1.76f, -0.55f), Quaternion.identity);
+            SpawnHazardTimer = 2f;
+        }
+    }
+}
