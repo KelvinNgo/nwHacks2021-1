bd9df12d98a62fe392dc72a1bc0d0352bf257290
Kelvin Ngo <kelvinngosos@gmail.com>
2021-01-09 17:30:56 -0800

    Enemy Shooting AI

---
+++ Assets/Scripts/PlantEnemyBullet.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantEnemyBullet : MonoBehaviour
+{
+    [SerializeField]
+    GameObject bullet;
+
+    float fireRate;
+    float nextFire;
+
+    void Start()
+    {
+        fireRate = 5f;
+        nextFire = Time.time;
+    }
+
+    void Update()
+    {
+        CheckIfTimeTOFire();
+    }
+
+    void CheckIfTimeToFire()
+    {
+        if(Time.time > nextFire) {
+            Instantiate (bullet, transformposition, Quaternion.identity);
+            nextFire = Time.time + fireRate
+        }
+    }
+}
